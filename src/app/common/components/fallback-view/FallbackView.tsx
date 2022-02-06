81d03e09664fe430e62ad9a04feb0def01cb7d8a
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-06 18:01:33 +0530

    loader

---
+++ src/app/common/components/fallback-view/FallbackView.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Rings } from 'react-loader-spinner';
+import { LoaderWrapper } from "./style";
+
+
+const Loader: FC = () => {
+  return <LoaderWrapper>
+    <Rings color="blue" width={100} height={100} />
+  </LoaderWrapper>
+}
+
+export default Loader;
