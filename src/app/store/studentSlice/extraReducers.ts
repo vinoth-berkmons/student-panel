015b7272790939fd0c7d31ec2325e875e819496f
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-07 23:12:53 +0530

    extra reducer to handle api response

---
+++ src/app/store/studentSlice/extraReducers.ts
@@ -0,0 +1,25 @@
+import { ActionReducerMapBuilder } from '@reduxjs/toolkit';
+
+import { StudentState } from '../../common/models/Student';
+import * as CONSTANTS from '../appConstants';
+import { doFetchStudents } from './thunk';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (builder: ActionReducerMapBuilder<StudentState>) {
+  builder
+    .addCase(doFetchStudents.pending, (state: StudentState, {payload}) => {
+      state.loading = true;
+      state.students = null;
+      state.status = CONSTANTS.STORE_STATUS.BUSY;
+    })
+    .addCase(doFetchStudents.rejected, (state: StudentState, {payload}) => {
+      state.status = CONSTANTS.STORE_STATUS.ERROR;
+      state.loading = false;
+    })
+    .addCase(doFetchStudents.fulfilled, (state: StudentState, { payload }) => {
+      console.log({payload})
+      state.students = payload;
+      state.status = CONSTANTS.STORE_STATUS.IDLE;
+      state.loading = false;
+    });
+}
