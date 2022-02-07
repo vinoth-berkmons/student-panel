59554d45f2bdd888ee3d6c5df1a2142a039aa779
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-07 23:13:11 +0530

    action constants

---
+++ src/app/store/studentSlice/constants.ts
@@ -0,0 +1,7 @@
+
+/**
+ * All the student related store const variables
+ */
+export const STUDENT_SLICE = "student";
+export const FETCH_STUDENTS = `${STUDENT_SLICE}/FETCH_STUDENTS`;
+export const FETCH_STUDENT_DETAIL = `${STUDENT_SLICE}/FETCH_STUDENT_DETAIL`;
