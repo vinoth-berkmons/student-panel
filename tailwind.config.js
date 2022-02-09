41de3d53af50efc57a7bdb5aa6a538632f474add
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-09 14:24:15 +0530

    packages added and custom screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xxsm': '320px',
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
