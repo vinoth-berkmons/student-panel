c789510e697cfac048b7d454f9bf4e2e8a0e7624
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-10 20:12:41 +0530

    consts

---
+++ src/app/store/coursesSlice/constants.ts
@@ -0,0 +1,7 @@
+
+/**
+ * All the Course related store const variables
+ */
+ export const COURSES_SLICE = "course";
+ export const FETCH_COURSES = `${COURSES_SLICE}/FETCH_COURSES`;
+ export const CREATE_COURSE = `${COURSES_SLICE}/CREATE_COURSE`;
