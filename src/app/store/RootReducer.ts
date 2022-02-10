59cc678807c13d972b5edf1a83bee1afa2ab731d
vinoth-berkmons <vinothberkmons@gmail.com>
2022-02-10 20:12:28 +0530

    root slice and consts

--- src/app/store/RootReducer.ts
+++ src/app/store/RootReducer.ts
@@ -1,18 +1,23 @@
 import { combineReducers } from "redux";
+import authSlice from "./auth/authSlice";
+import coursesSlice from "./coursesSlice/coursesSlice";
+import createCourseSlice from "./coursesSlice/createCourseSlice";
 import studentDetailSlice from "./studentSlice/studentDetailSlice";
 import studentSlice from "./studentSlice/studentSlice";
 
 /**
- * All the common reducers placed in store
- * Other reducers are placed in respective directory
+ * All the  reducers placed in store
  */
 
 /**
  * Root reducer holds all the reducer of the app
  */
 export const rootReducer = combineReducers({
+  auth: authSlice,
   students: studentSlice,
   student: studentDetailSlice,
+  courses: coursesSlice,
+  createCourse: createCourseSlice
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
